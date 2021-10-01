ff533c9d2dbc14c4e0f90395c39a374ea1157b49
ZURIZAL MUHAMMAD <72717423+ZU2H@users.noreply.github.com>
2021-10-01 21:30:05 +0700

    Mengupload file

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $HOST = "127.0.0.1";
+    $USER = "root";
+    $PASSWORD = "";
+    $DATABASE = "dbrestoran";
+
+    $koneksi = mysqli_connect($HOST, $USER, $PASSWORD, $DATABASE);
+
+?>
